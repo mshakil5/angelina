619ec5871fb5bd1fe19cf8a3dfd4a5e90bf4c4ec
shakil <46122677+mshakil5@users.noreply.github.com>
2026-09-22 14:44:42 +0600

    team

--- resources/views/frontend/agegroup.blade.php
+++ resources/views/frontend/agegroup.blade.php
@@ -72,11 +72,11 @@
 <section class="breadcrumb-section text-center text-white d-flex align-items-center justify-content-center"
     style="background-image: url('{{ $bgImage }}');">
   <div class="container  d-none">
-    <h1 class="breadcrumb-title mb-3">{{ $agegroup->short_title }}</h1>
+    <h1 class="breadcrumb-title mb-3">{{ $agegroup->short_title ?? "" }}</h1>
     <nav aria-label="breadcrumb">
       <ol class="breadcrumb justify-content-center mb-0">
         <li class="breadcrumb-item"><a href="{{route('home')}}" class="text-white text-decoration-none">Home</a></li>
-        <li class="breadcrumb-item active text-white" aria-current="page">{{ $agegroup->short_title }}</li>
+        <li class="breadcrumb-item active text-white" aria-current="page">{{ $agegroup->short_title  ?? ""  }}</li>
       </ol>
     </nav>
   </div>
